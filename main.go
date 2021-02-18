a546fac4fd713778bfbe07a94e30114c3dfb8bcf
Alexander Lagerström <alexander.lagerstrom@couchbase.com>
2021-02-18 12:52:44 +0100

    Removes comments

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"time"
 	"github.com/lagerstrom/go-forward/forwarder"
 )
 
@@ -11,9 +10,4 @@ func main() {
 	go forwarder.StartServer()
 	//http://stackoverflow.com/questions/9543835/how-best-do-i-keep-a-long-running-go-program-running
 	select {} // block forever, this program exits by ctrl+c :)
-	//give the server a moment to start
-	//time.Sleep(100 * time.Millisecond)
-	//go forwarder.SendTestMsg(done)
-	//wait for client to receive response from server
-	//<- done
 }
